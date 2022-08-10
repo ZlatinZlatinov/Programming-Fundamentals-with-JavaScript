a68094130d175c8115c2e5be02a479d55947febb
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2022-08-10 12:00:14 +0300

    file upload

---
+++ 1.1 Basic Syntax - Exercise/08multiplicationTable.js
@@ -0,0 +1,9 @@
+function multiplicationTable(num) { 
+    let sum = 0;
+     for (let x = 1; x <= 10; x++){ 
+         sum = num*x;
+         console.log(`${num} X ${x} = ${sum}`);
+     }
+} 
+multiplicationTable(5); 
+// 100/100 ezzz
