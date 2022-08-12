809a53e4aaf3a2628268685b55731b04e67988d5
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2022-08-12 13:20:38 +0300

    file upload

---
+++ 2.1 Data Types - Exercise/03townInfo.js
@@ -0,0 +1,5 @@
+function townInfo(townName, population, areaNum) {
+    console.log(`Town ${townName} has population of ${population} and area ${areaNum} square km.`);
+} 
+townInfo('Sofia',1286383,492); 
+// 100/100
