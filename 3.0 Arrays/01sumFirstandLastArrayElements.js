f8412d20ba0b9c47c03771df1adfed6c34b42103
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2022-08-13 23:30:23 +0300

    file upload

---
+++ 3.0 Arrays/01sumFirstandLastArrayElements.js
@@ -0,0 +1,8 @@
+function sumFirstandLastArrayElements (arrayy){
+    let first = arrayy[0]; 
+    let last = arrayy[arrayy.length-1]; 
+    let sum = first + last; 
+    console.log(sum);
+} 
+sumFirstandLastArrayElements([10, 20, 30, 40, 33]); 
+// 100/100
