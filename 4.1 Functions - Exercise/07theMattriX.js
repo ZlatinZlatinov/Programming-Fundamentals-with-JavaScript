e6304e112ceffdf733173d79652adfadea752239
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2022-08-16 16:43:01 +0300

    file upload

---
+++ 4.1 Functions - Exercise/07theMattriX.js
@@ -0,0 +1,12 @@
+function theMattriXNeo(num) {
+    for (let i = 0; i < num; i++) {
+        let newArray = [];
+        for (let k = 0; k < num; k++) {
+            newArray.push(num);
+        }
+        console.log(newArray.join(' '));
+    } 
+    //Mr. Anderseeeennn
+}
+theMattriXNeo(3); 
+// 100/100 simple as that
