88f9ee916b557af52325f5cb669c1f8d15e0ea40
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2022-08-29 20:59:05 +0300

    elif daolpu

---
+++ 9.0 Text Processing/02substring.js
@@ -0,0 +1,6 @@
+function substring (string, num1, num2) {
+    let result = string.substring(num1, num1+num2); 
+    console.log(result);
+} // 100/100
+substring('ASentence', 1, 8); 
+substring('SkipWord', 4, 7);
