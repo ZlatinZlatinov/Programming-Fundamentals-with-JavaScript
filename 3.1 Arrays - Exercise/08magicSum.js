0e7753b19d11211df127d8080c12052a66a3f4da
Zlatin Zlatinov <109311352+ZlatinZlatinov@users.noreply.github.com>
2024-02-17 13:21:28 +0200

    Update 08magicSum.js

--- 3.1 Arrays - Exercise/08magicSum.js
+++ 3.1 Arrays - Exercise/08magicSum.js
@@ -14,7 +14,7 @@ function magicSum(arri, num) {
 //magicSum([1, 7, 6, 2, 19, 23], 8);
 //magicSum([14, 20, 60, 13, 7, 19, 8], 27);
 //magicSum([1, 2, 3, 4, 5, 6], 6) // emm 83 / 100 not that bad 
-// pak taka de ama da xdd tup juj 
+
 
 
 function test (array){
